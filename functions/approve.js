5b5f3048013fa7263b19a0485eb72c2a023ffb26
thqhwdlylalbay <thqhwdlylalbay@gmail.com>
2026-04-01 10:11:05 +0200

    Update approve.js

--- functions/approve.js
+++ functions/approve.js
@@ -1,17 +1,16 @@
 export async function onRequestPost(context) {
-    const { request, env } = context;
-    const { paymentId } = await request.json();
-    
-    const res = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/approve`, {
-        method: "POST",
-        headers: {
-            "Authorization": `Key ${env.PI_API_KEY}`,
-            "Content-Type": "application/json"
-        }
-    });
-    
-    const data = await res.json();
-    return new Response(JSON.stringify(data), {
-        headers: { "Content-Type": "application/json" }
-    });
+  const { request, env } = context;
+  const { paymentId } = await request.json();
+  
+  const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/approve`, {
+    method: "POST",
+    headers: {
+      "Authorization": `Key ${env.PI_API_KEY}`,
+      "Content-Type": "application/json"
+    }
+  });
+  
+  return new Response(JSON.stringify(await response.json()), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
